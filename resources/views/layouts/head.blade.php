48f8f32a9886ac65712d644246306cc9bebf1c7f
Anees Siddique <aneessiddique21@gmail.com>
2024-03-26 00:11:49 +0500

    test changes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -14,7 +14,6 @@
 	<!-- Responsive CSS -->
 	<link rel="stylesheet" href="/assets/css/responsive.css">
 
-
 	<!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" /> -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
